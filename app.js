942caac36846da9a98359bc65ce4b2a59857cc7f
Hashmatullah Nazari <hn0153@gmail.com>
2022-04-05 16:56:22 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  document.getElementById("myDIV").style.backgroundColor = "#fadcc4";
+  document.getElementById("myDIV").style.transition = "all 2s";
+}
+function myFunction1() {
+  document.getElementById("myDIV").style.backgroundColor = "#9a8628";
+  document.getElementById("myDIV").style.transition = "all 2s";
+}
+function myFunction2() {
+  document.getElementById("myDIV").style.backgroundColor = "#d37643";
+  document.getElementById("myDIV").style.transition = "all 2s";
+}
+function myFunction3() {
+  document.getElementById("myDIV").style.backgroundColor = "#affba2";
+  document.getElementById("myDIV").style.transition = "all 2s";
+}
